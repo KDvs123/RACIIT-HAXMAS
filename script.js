7dfbdc3bc11b10232246131afeedf6cb18a3ede3
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-07-27 05:03:12 +0530

    did the error handling on the timeline section

--- script.js
+++ script.js
@@ -9,15 +9,15 @@
         });
 
         var agTimeline = $('.js-timeline'),
-        agTimelineLine = $('.js-timeline-item'),
-        agTimelineLineProgress = $('.js-timeline_line-progress'),
-        agTimelinePoint=$('.js-timeline-card_point-box'),
-        agTimelineItem=$('.js-timeline_item'),
-        agOuterHeight=$(window).outerHeight(),
-        agHeight= $(window).height(),
-        f= -1,
-        agFlag=false;
-        
+            agTimelineLine = $('.js-timeline_line'),
+            agTimelineLineProgress = $('.js-timeline_line-progress'),
+            agTimelinePoint=$('.js-timeline-card_point-box'),
+            agTimelineItem=$('.js-timeline_item'),
+            agOuterHeight=$(window).outerHeight(),
+            agHeight= $(window).height(),
+            f= -1,
+            agFlag=false;
+            
 
         function fnOnScroll(){
              agPosY = $(window).scrollTop();
@@ -45,14 +45,14 @@
             var agTop= agTimelineItem.last().find(agTimelinePoint).offset().top;
 
             i = agTop + agPosY - $(window).scrollTop();
-            a = agTimelineLineProgress.offset().top+ agPosY - $(window).scrollTop();
-            n = agPosY - a+ agOuterHeight/2;
-            i<= agPosY + agOuterHeight/2 && (n=i-a);
-            agTimelineLineProgress.css({height: n+ "px"});
+            a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+            n = agPosY - a + agOuterHeight/2;
+            i <= agPosY + agOuterHeight/2 && (n= i-a);
+            agTimelineLineProgress.css({height: n + "px"});
 
             agTimelineItem.each(function(){
                 var agTop= $(this).find(agTimelinePoint).offset().top;
-                (agTop + agPosY - $(window).scrollTop() < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active'));
+                (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
             })
         }
 
